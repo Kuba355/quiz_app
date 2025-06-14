5936c6f228407ceeb35c94afba4304e7d2a1b6bf
MichalSadlowski <136119697+MichalSadlowski@users.noreply.github.com>
2025-06-14 14:05:14 +0200

    Update data.sql - new questions

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -67,3 +67,143 @@ INSERT INTO question_options (question_id, options) VALUES (10, 'Afryka');
 INSERT INTO question_options (question_id, options) VALUES (10, 'Azja');
 INSERT INTO question_options (question_id, options) VALUES (10, 'Europa');
 INSERT INTO question_options (question_id, options) VALUES (10, 'Ameryka Południowa');
+
+-- Pytanie 11
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (11, 'Jakie jest najwyższe pasmo górskie na świecie?', 0, 'geografia');
+INSERT INTO question_options (question_id, options) VALUES (11, 'Himalaje');
+INSERT INTO question_options (question_id, options) VALUES (11, 'Andes');
+INSERT INTO question_options (question_id, options) VALUES (11, 'Alpy');
+INSERT INTO question_options (question_id, options) VALUES (11, 'Kordyliery');
+
+-- Pytanie 12
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (12, 'Które morze jest najbardziej zasolone?', 1, 'geografia');
+INSERT INTO question_options (question_id, options) VALUES (12, 'Morze Śródziemne');
+INSERT INTO question_options (question_id, options) VALUES (12, 'Morze Martwe');
+INSERT INTO question_options (question_id, options) VALUES (12, 'Morze Bałtyckie');
+INSERT INTO question_options (question_id, options) VALUES (12, 'Morze Czarne');
+
+-- Pytanie 13
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (13, 'Które państwo ma największą powierzchnię?', 0, 'geografia');
+INSERT INTO question_options (question_id, options) VALUES (13, 'Rosja');
+INSERT INTO question_options (question_id, options) VALUES (13, 'Kanada');
+INSERT INTO question_options (question_id, options) VALUES (13, 'Chiny');
+INSERT INTO question_options (question_id, options) VALUES (13, 'USA');
+
+-- Pytanie 14
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (14, 'Przez ile państw przepływa Amazonka?', 2, 'geografia');
+INSERT INTO question_options (question_id, options) VALUES (14, '2');
+INSERT INTO question_options (question_id, options) VALUES (14, '3');
+INSERT INTO question_options (question_id, options) VALUES (14, '7');
+INSERT INTO question_options (question_id, options) VALUES (14, '5');
+
+-- Pytanie 15
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (15, 'Stolica Australii to?', 3, 'geografia');
+INSERT INTO question_options (question_id, options) VALUES (15, 'Sydney');
+INSERT INTO question_options (question_id, options) VALUES (15, 'Melbourne');
+INSERT INTO question_options (question_id, options) VALUES (15, 'Perth');
+INSERT INTO question_options (question_id, options) VALUES (15, 'Canberra');
+
+-- Pytanie 16
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (16, 'Ile wynosi pierwiastek kwadratowy z 144?', 2, 'matematyka');
+INSERT INTO question_options (question_id, options) VALUES (16, '10');
+INSERT INTO question_options (question_id, options) VALUES (16, '11');
+INSERT INTO question_options (question_id, options) VALUES (16, '12');
+INSERT INTO question_options (question_id, options) VALUES (16, '14');
+
+-- Pytanie 17
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (17, 'Ile stopni ma kąt prosty?', 0, 'matematyka');
+INSERT INTO question_options (question_id, options) VALUES (17, '90');
+INSERT INTO question_options (question_id, options) VALUES (17, '45');
+INSERT INTO question_options (question_id, options) VALUES (17, '180');
+INSERT INTO question_options (question_id, options) VALUES (17, '60');
+
+-- Pytanie 18
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (18, 'Jaka jest liczba π (pi) zaokrąglona do dwóch miejsc po przecinku?', 3, 'matematyka');
+INSERT INTO question_options (question_id, options) VALUES (18, '3.12');
+INSERT INTO question_options (question_id, options) VALUES (18, '3.20');
+INSERT INTO question_options (question_id, options) VALUES (18, '3.00');
+INSERT INTO question_options (question_id, options) VALUES (18, '3.14');
+
+-- Pytanie 19
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (19, 'Jaka jest suma kątów wewnętrznych trójkąta?', 0, 'matematyka');
+INSERT INTO question_options (question_id, options) VALUES (19, '180');
+INSERT INTO question_options (question_id, options) VALUES (19, '360');
+INSERT INTO question_options (question_id, options) VALUES (19, '90');
+INSERT INTO question_options (question_id, options) VALUES (19, '120');
+
+-- Pytanie 20
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (20, 'Ile to 7 * 8?', 1, 'matematyka');
+INSERT INTO question_options (question_id, options) VALUES (20, '54');
+INSERT INTO question_options (question_id, options) VALUES (20, '56');
+INSERT INTO question_options (question_id, options) VALUES (20, '58');
+INSERT INTO question_options (question_id, options) VALUES (20, '64');
+
+-- Pytanie 21
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (21, 'Która planeta jest najbliżej Słońca?', 0, 'astronomia');
+INSERT INTO question_options (question_id, options) VALUES (21, 'Merkury');
+INSERT INTO question_options (question_id, options) VALUES (21, 'Wenus');
+INSERT INTO question_options (question_id, options) VALUES (21, 'Ziemia');
+INSERT INTO question_options (question_id, options) VALUES (21, 'Mars');
+
+-- Pytanie 22
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (22, 'Ile księżyców ma Ziemia?', 0, 'astronomia');
+INSERT INTO question_options (question_id, options) VALUES (22, '1');
+INSERT INTO question_options (question_id, options) VALUES (22, '2');
+INSERT INTO question_options (question_id, options) VALUES (22, '3');
+INSERT INTO question_options (question_id, options) VALUES (22, '0');
+
+-- Pytanie 23
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (23, 'Która planeta jest największa w Układzie Słonecznym?', 2, 'astronomia');
+INSERT INTO question_options (question_id, options) VALUES (23, 'Ziemia');
+INSERT INTO question_options (question_id, options) VALUES (23, 'Saturn');
+INSERT INTO question_options (question_id, options) VALUES (23, 'Jowisz');
+INSERT INTO question_options (question_id, options) VALUES (23, 'Neptun');
+
+-- Pytanie 24
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (24, 'Jak nazywa się galaktyka, w której znajduje się Ziemia?', 1, 'astronomia');
+INSERT INTO question_options (question_id, options) VALUES (24, 'Andromeda');
+INSERT INTO question_options (question_id, options) VALUES (24, 'Droga Mleczna');
+INSERT INTO question_options (question_id, options) VALUES (24, 'Messier 87');
+INSERT INTO question_options (question_id, options) VALUES (24, 'Wielki Obłok Magellana');
+
+-- Pytanie 25
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (25, 'Która planeta ma pierścienie?', 1, 'astronomia');
+INSERT INTO question_options (question_id, options) VALUES (25, 'Mars');
+INSERT INTO question_options (question_id, options) VALUES (25, 'Saturn');
+INSERT INTO question_options (question_id, options) VALUES (25, 'Wenus');
+INSERT INTO question_options (question_id, options) VALUES (25, 'Merkury');
+
+-- Pytanie 26
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (26, 'Jaki symbol chemiczny ma wodór?', 0, 'chemia');
+INSERT INTO question_options (question_id, options) VALUES (26, 'H');
+INSERT INTO question_options (question_id, options) VALUES (26, 'O');
+INSERT INTO question_options (question_id, options) VALUES (26, 'W');
+INSERT INTO question_options (question_id, options) VALUES (26, 'He');
+
+-- Pytanie 27
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (27, 'Jaki pierwiastek ma symbol Fe?', 1, 'chemia');
+INSERT INTO question_options (question_id, options) VALUES (27, 'Fluor');
+INSERT INTO question_options (question_id, options) VALUES (27, 'Żelazo');
+INSERT INTO question_options (question_id, options) VALUES (27, 'Fosfor');
+INSERT INTO question_options (question_id, options) VALUES (27, 'Ferm');
+
+-- Pytanie 28
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (28, 'Co jest głównym składnikiem powietrza?', 2, 'chemia');
+INSERT INTO question_options (question_id, options) VALUES (28, 'Tlen');
+INSERT INTO question_options (question_id, options) VALUES (28, 'Dwutlenek węgla');
+INSERT INTO question_options (question_id, options) VALUES (28, 'Azot');
+INSERT INTO question_options (question_id, options) VALUES (28, 'Wodór');
+
+-- Pytanie 29
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (29, 'Jakie jest pH czystej wody?', 3, 'chemia');
+INSERT INTO question_options (question_id, options) VALUES (29, '4');
+INSERT INTO question_options (question_id, options) VALUES (29, '2');
+INSERT INTO question_options (question_id, options) VALUES (29, '10');
+INSERT INTO question_options (question_id, options) VALUES (29, '7');
+
+-- Pytanie 30
+INSERT INTO question (id, content, correct_answer_index, category) VALUES (30, 'Który gaz jest używany w balonach?', 2, 'chemia');
+INSERT INTO question_options (question_id, options) VALUES (30, 'Tlen');
+INSERT INTO question_options (question_id, options) VALUES (30, 'Wodór');
+INSERT INTO question_options (question_id, options) VALUES (30, 'Hel');
+INSERT INTO question_options (question_id, options) VALUES (30, 'Azot');
